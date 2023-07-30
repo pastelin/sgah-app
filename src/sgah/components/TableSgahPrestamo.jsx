bc0e7ed78a752644bc48c0a42e4afd74eea329c6
Juan Pastelin <pastelin406@gmail.com>
2023-07-30 17:34:56 -0500

    fix: Fijar thead de las tablas
    Utiliza estilo sticky para fijar la cabecera al llegar a la posición indicada

--- src/sgah/components/TableSgahPrestamo.jsx
+++ src/sgah/components/TableSgahPrestamo.jsx
@@ -1,51 +1,47 @@
 import { formatCurrency, useSgahPrestamoStore } from '../../hooks';
 
 export const TableSgahPrestamo = () => {
-    const { prestamos, startLoadingPrestamo } = useSgahPrestamoStore();
+	const { prestamos, startLoadingPrestamo } = useSgahPrestamoStore();
 
 	const handleOpenFormUpdatePrestamo = (folio) => {
 		if (!!folio) {
-            startLoadingPrestamo(folio);
+			startLoadingPrestamo(folio);
 		}
 	};
 
 	return (
-		<>
-			<div className="contenedor__table">
-				<table>
-					<thead>
-						<tr>
-							<th>Fecha</th>
-							<th>Saldo utilizado</th>
-							<th>Descripción</th>
-							<th>Deuda Actual</th>
-							<th></th>
-						</tr>
-					</thead>
+		<div className="contenedor__table">
+			<table>
+				<thead>
+					<tr>
+						<th>Fecha</th>
+						<th>Saldo utilizado</th>
+						<th>Descripción</th>
+						<th>Deuda Actual</th>
+						<th></th>
+					</tr>
+				</thead>
 
-					<tbody>
-						{prestamos.map((prestamo) => (
-							<tr key={window.crypto.getRandomValues(new Uint32Array(1))[0]}>
-								<td>{prestamo.fechaCreacion}</td>
-								<td>{formatCurrency(prestamo.montoPrestado)}</td>
-								<td>{prestamo.descripcion}</td>
-								<td>
-									{formatCurrency(prestamo.montoPrestado - prestamo.montoPagado)}
-								</td>
-								<td>
-									<a
-										onClick={() => handleOpenFormUpdatePrestamo(prestamo.folio)}
-										className="btn-prestamos"
-									>
-										Pagar
-									</a>
-								</td>
-								<td></td>
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</div>
-		</>
+				<tbody>
+					{prestamos.map((prestamo) => (
+						<tr key={window.crypto.getRandomValues(new Uint32Array(1))[0]}>
+							<td>{prestamo.fechaCreacion}</td>
+							<td>{formatCurrency(prestamo.montoPrestado)}</td>
+							<td>{prestamo.descripcion}</td>
+							<td>{formatCurrency(prestamo.montoPrestado - prestamo.montoPagado)}</td>
+							<td>
+								<a
+									onClick={() => handleOpenFormUpdatePrestamo(prestamo.folio)}
+									className="btn-prestamos"
+								>
+									Pagar
+								</a>
+							</td>
+							<td></td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
 	);
 };
